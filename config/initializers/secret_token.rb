b48654054ed4bf65c72354e8d30f894891a48e3a
Logan Hasson <logan.hasson@gmail.com>
2013-12-22 09:25:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PuppyVote::Application.config.secret_key_base = '385190a4bc4ce49669d465e8e8a85b23e78d53f207b666d23fb4f7306c43edfd02071a413a30b77eef0c3a318c75abbbf35d29ce4df0468c12a4e918b75cafd4'
